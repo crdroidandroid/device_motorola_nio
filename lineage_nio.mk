8d3e2dbb726dec8753a27af95f4e09017e8e48e5
vulkan-ops <vulkanops@gmail.com>
2024-02-17 16:02:05 -0300

    nio: just comment BUILD_FINGERPRINT
    Signed-off-by: vullk4n <vulkanops@gmail.com>

--- lineage_nio.mk
+++ lineage_nio.mk
@@ -20,11 +20,10 @@ PRODUCT_DEVICE := nio
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := XT2125-4
 PRODUCT_MANUFACTURER := motorola
-
 PRODUCT_SYSTEM_NAME := nio_retcn
 
 # Build info
-BUILD_FINGERPRINT := "motorola/nio_retcn/nio:12/S1RN32.55-16-13/fce21a-b9f4e9:user/release-keys"
+#BUILD_FINGERPRINT := "motorola/nio_retcn/nio:12/S1RN32.55-16-13/fce21a-b9f4e9:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="nio_retcn-user 12 S1RN32.55-16-13 fce21a-b9f4e9 release-keys" \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
