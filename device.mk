9b8e9d78663fa01565c2905d9ddc7bffb67048ca
vullk4n <vulkanops@gmail.com>
2024-06-16 00:03:17 -0300

    nio: don't build AudioFx
    Signed-off-by: vullk4n <vulkanops@gmail.com>

--- device.mk
+++ device.mk
@@ -25,6 +25,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
+TARGET_EXCLUDES_AUDIOFX := true
+
 # Init
 PRODUCT_PACKAGES += \
     init.device.rc
